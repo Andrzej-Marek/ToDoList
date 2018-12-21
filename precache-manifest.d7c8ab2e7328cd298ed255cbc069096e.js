c8b9cd9bc2696a863bce90b73d4eec4efbeb9e9b
System Administrator <root@tests-MacBook-Pro.local>
2018-12-21 15:20:36 +0100

    Updates

---
+++ precache-manifest.d7c8ab2e7328cd298ed255cbc069096e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8ce39769359522e24a1f24055558f15",
+    "url": "/ToDoList/static/media/GoodJob.f8ce3976.gif"
+  },
+  {
+    "revision": "b0420226dc1dc1a6d39c",
+    "url": "/ToDoList/static/js/runtime~main.b0420226.js"
+  },
+  {
+    "revision": "48c65b82a372870b9be3",
+    "url": "/ToDoList/static/js/main.48c65b82.chunk.js"
+  },
+  {
+    "revision": "465be544234efc518e30",
+    "url": "/ToDoList/static/js/1.465be544.chunk.js"
+  },
+  {
+    "revision": "48c65b82a372870b9be3",
+    "url": "/ToDoList/static/css/main.a01b688b.chunk.css"
+  },
+  {
+    "revision": "28183f9c366e75791686644c1ca7f4eb",
+    "url": "/ToDoList/index.html"
+  }
+];
